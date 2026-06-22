d19f1ac3c0d0d6e9101e9605cba384575417fd41
Ankita <ankietathorve@gmail.com>
2026-06-22 21:52:04 +0530

    Variables

--- week1-Foundations/Day1/variables.js
+++ week1-Foundations/Day1/variables.js
@@ -1,6 +1,6 @@
-let totalPoints = 10;
-totalPoints = 15; // allowed
-console.log(totalPoints);
+let totalPrice = 10;
+totalPrice = 15; // allowed
+console.log(totalPrice);
 
 const pi = 3.14159;
 // pi = 3.14; //Throws a TypeError
