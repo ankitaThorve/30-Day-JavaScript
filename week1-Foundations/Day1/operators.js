fe6aad1301ca79bb402335efa8d261a0e99b75f7
Ankita <ankietathorve@gmail.com>
2026-06-22 23:17:52 +0530

    Arithmatic Operators

---
+++ week1-Foundations/Day1/operators.js
@@ -0,0 +1,18 @@
+// Arithmatic Operators
+let add = 4 + 5; // addition
+console.log(add);
+
+let substract = 4 - 5; // substraction
+console.log(substract);
+
+let multiply = 4 * 5; // multiplication
+console.log(multiply);
+
+let divide = 4 / 5; // division
+console.log(divide);
+
+let remainder = 4 % 5; // remainder / modulus
+console.log(remainder);
+
+let exponentiation = 4 ** 5;
+console.log(exponentiation)
