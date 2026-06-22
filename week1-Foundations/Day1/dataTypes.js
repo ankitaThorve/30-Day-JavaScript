0342e0883a7c1b1287ac427a510142b6cdaf9663
Ankita <ankietathorve@gmail.com>
2026-06-22 23:09:52 +0530

    Data Types

---
+++ week1-Foundations/Day1/dataTypes.js
@@ -0,0 +1,17 @@
+let number = 42;
+console.log(typeof number); // type of a value
+
+let string = "hello";
+console.log(typeof string);
+
+let boolean = true;
+console.log(typeof boolean);
+
+let notInitialised = undefined;
+console.log(typeof notInitialised);
+
+let absentValue = null;
+console.log(typeof absentValue);
+
+console.log(typeof Symbol);
+
