8425e4e2108053d24cdf7074bb21272774ba309e
Ankita <ankietathorve@gmail.com>
2026-06-22 23:25:39 +0530

    Assignment Operators

--- week1-Foundations/Day1/operators.js
+++ week1-Foundations/Day1/operators.js
@@ -15,4 +15,21 @@ let remainder = 4 % 5; // remainder / modulus
 console.log(remainder);
 
 let exponentiation = 4 ** 5;
-console.log(exponentiation)
+console.log(exponentiation);
+
+// Assignment Operators
+
+let x = 1; // assignment
+let y = 2;
+
+x += y;
+console.log(x); // addition assignment x = x + y
+
+x -= y;
+console.log(x); //substraction assignment
+
+x *= y;
+console.log(x); //multiplication assignment
+
+x /= y;
+console.log(x); //division assignment
