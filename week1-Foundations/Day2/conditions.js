3349d9d9cf952dd10bd9c8f7ef31218cab132043
Ankita <ankietathorve@gmail.com>
2026-06-23 22:59:44 +0530

    conditional(ternary) operator

--- week1-Foundations/Day2/conditions.js
+++ week1-Foundations/Day2/conditions.js
@@ -24,3 +24,8 @@ switch (fruit) {
   default:
     console.log("Unknown fruit");
 }
+
+// conditional(Ternary) operator
+let age = 20;
+let status = age >= 18 ? "Adult" : "Minor";
+console.log(status);
