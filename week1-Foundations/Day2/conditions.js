0e4190568764cdd5125cb4f5d39547c45c2affca
Ankita <ankietathorve@gmail.com>
2026-06-23 22:36:33 +0530

    if...else Statement

---
+++ week1-Foundations/Day2/conditions.js
@@ -0,0 +1,11 @@
+// if...else statement
+
+let time = 14;
+
+if (time < 12) {
+  console.log("Good morning!");
+} else if (time < 18) {
+  console.log("Good afternoon!"); // This block runs
+} else {
+  console.log("Good evening!");
+}
