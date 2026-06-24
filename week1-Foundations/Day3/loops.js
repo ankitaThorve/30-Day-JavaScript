fafdd5430ec17eb62dbfd548419dd46c21194e26
Ankita <ankietathorve@gmail.com>
2026-06-24 19:59:17 +0530

    for loop

---
+++ week1-Foundations/Day3/loops.js
@@ -0,0 +1,8 @@
+// for loop
+// Initialization: Executes once before the loop starts to declare or initialize a counter variable
+// .Condition: Evaluated before every iteration. If true, the code block runs; if false, the loop stops.
+// Afterthought: Executes at the end of each iteration loop (usually to increment or decrement the counter).
+
+for (let i = 0; i < 3; i++) {
+  console.log(i); //0,1,2
+}
