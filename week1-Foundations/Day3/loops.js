e239e41642cec5e2bbc80f627aece82ad6ac9f95
Ankita <ankietathorve@gmail.com>
2026-06-24 20:13:59 +0530

    while loop

--- week1-Foundations/Day3/loops.js
+++ week1-Foundations/Day3/loops.js
@@ -1,8 +1,15 @@
-// for loop
-// Initialization: Executes once before the loop starts to declare or initialize a counter variable
+// for loop----
+// Initialization: Runs once before the loop starts to set up a counter variable.
 // .Condition: Evaluated before every iteration. If true, the code block runs; if false, the loop stops.
 // Afterthought: Executes at the end of each iteration loop (usually to increment or decrement the counter).
 
 for (let i = 0; i < 3; i++) {
   console.log(i); //0,1,2
+}
+
+// while statement ---
+let count = 0;
+while (count < 3) {
+  console.log(count);
+  count++; 
 }
