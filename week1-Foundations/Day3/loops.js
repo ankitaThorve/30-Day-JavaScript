5a9a5048dffbc53dbf37629f39a26b1fc92d55ff
Ankita <ankietathorve@gmail.com>
2026-06-25 16:34:52 +0530

    continue statement

--- week1-Foundations/Day3/loops.js
+++ week1-Foundations/Day3/loops.js
@@ -21,3 +21,15 @@ const fruits = ["apple", "banana", "cherry"];
 for (const fruit of fruits) {
   console.log(fruit);
 }
+
+// Continue statement
+
+let text = "";
+
+for (let i = 0; i < 10; i++) {
+  if (i === 3) {
+    continue;
+  }
+  text += i;
+}
+console.log(text);
