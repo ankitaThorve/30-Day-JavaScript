b85792622dadb16adfde358e2247bdcdb88152d9
Ankita <ankietathorve@gmail.com>
2026-06-26 19:31:02 +0530

    Fully Customized Parameters

--- week1-Foundations/Day4/FizzBuzz.js
+++ week1-Foundations/Day4/FizzBuzz.js
@@ -35,3 +35,20 @@ function getFizzBuzzArray(n) {
 
 const output = getFizzBuzzArray(5);
 console.log(output);
+
+// Fully Customized Parameters (Advanced)---
+
+// Parameter list handles boundaries and dynamic words
+function customFizzBuzz(start, end, word1, divisor1, word2, divisor2) {
+  for (let i = start; i <= end; i++) {
+    let customOutput = "";
+
+    if (i % divisor1 === 0) customOutput += word1;
+    if (i % divisor2 === 0) customOutput += word2;
+
+    console.log(customOutput || i);
+  }
+}
+// Executing with unique rules: 7 replaces with "Foo", 2 replaces with "Bar"
+
+console.log(customFizzBuzz(1, 10, "Foo", 7, "Bar", 2));
