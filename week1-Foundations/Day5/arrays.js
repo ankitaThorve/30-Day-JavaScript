d0c8f76eca17b87c16054ab5b901185198cdfa46
Ankita <ankietathorve@gmail.com>
2026-07-01 18:34:16 +0530

    Arrays push() method

--- week1-Foundations/Day5/arrays.js
+++ week1-Foundations/Day5/arrays.js
@@ -1,2 +1,11 @@
 // an Array is a global object used to store ordered collections of data.
 
+// The push() method adds one or more elements directly to the end of an array.Behavior: It is a mutating method, meaning it directly modifies the original array rather than creating a new one.Return Value: It returns the new length property of the mutated array.
+
+const fruits = ["apple", "banana"];
+
+// Adding a single element
+const newLength = fruits.push("orange");
+
+console.log(fruits);
+console.log(newLength);
