58ffca348ef3fc2486de65683a7369075cc1443f
Ankita <ankietathorve@gmail.com>
2026-07-10 20:48:35 +0530

    Empty object

--- week1-Foundations/Day6/objects.js
+++ week1-Foundations/Day6/objects.js
@@ -4,3 +4,13 @@ const user = { name: "Alice", age: 30 };
 // type, model, and color are properties
 // "Fiat", "500", and "white" are property values
 const car = { type: "Fiat", model: "500", color: "white" };
+
+// can also create an empty object, and add the properties later:
+const person = {};
+
+person.firstName = "John";
+person.lastName = "Carter";
+person.age1 = 28;
+person.eyeColor = "blue";
+
+console.log(person);
