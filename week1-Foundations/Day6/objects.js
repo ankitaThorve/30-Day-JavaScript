01fe780ada7edabf84cc682d04cb023e64cc5ee1
Ankita <ankietathorve@gmail.com>
2026-07-10 20:42:21 +0530

    Properties & property values

--- week1-Foundations/Day6/objects.js
+++ week1-Foundations/Day6/objects.js
@@ -1,4 +1,6 @@
 // Creating objects
 const user = { name: "Alice", age: 30 };
 
+// type, model, and color are properties
+// "Fiat", "500", and "white" are property values
 const car = { type: "Fiat", model: "500", color: "white" };
